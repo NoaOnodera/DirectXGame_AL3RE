97e85c3b75ed3ba72b7f353880ead92ddec9866a
乃碧 小野寺 <deranoir102493@gmail.com>
2022-05-24 13:40:38 +0900

    確認課題02_02_RE

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -1,13 +1,12 @@
 ﻿#include "GameScene.h"
 #include "TextureManager.h"
 #include "AxisIndicator.h"
-#include "PrimitiveDrawer.h"
 #include <cassert>
 
 GameScene::GameScene() {}
 
 GameScene::~GameScene() {
-	delete model_;
+	
 	delete debugCamera_;
 }
 
@@ -22,65 +21,100 @@ void GameScene::Initialize() {
 	debugCamera_ = new DebugCamera(1280, 720);
 	AxisIndicator::GetInstance()->SetVisible(true);//軸方向表示の表示を有効にする
 	AxisIndicator::GetInstance()->SetTargetViewProjection(&debugCamera_->GetViewProjection());//軸方向表示が参照するビュープロジェクションを指定する(アドレス渡し)
+	worldTransform_.Initialize();
+	viewProjection_.Initialize();
+	
+	
+	
+	
+#pragma region スケーリング
 	//スケーリング
-	worldTransform_.scale_ = {5,1,1};//x,y,z方向のスケーリングを設定
-	worldTransform_.rotation_ = { 0.0f,0.0f,1.0f };//x,y,z軸の周りの回転角を設定
-	worldTransform_.rotation_ = { 1.0f,0.0f,0.0f };
-	worldTransform_.rotation_ = { 0.0f,1.0f,0.0f };
-	worldTransform_.translation_ = { 0.0f,0.0f,0.0f };
+	worldTransform_.scale_ = { 5,5,5 };//x,y,z方向のスケーリングを設定
 	Matrix4 matScale;//スケーリング行列を宣言
-	Matrix4 matRotZ;//Z軸回転行列を宣言
-	Matrix4 matRotX;//X軸回転行列を宣言
-	Matrix4 matRotY;//Y軸回転行列を宣言
+	//スケーリング倍率を行列に設定する
+	matScale.m[0][0] = worldTransform_.scale_.x;
+	matScale.m[1][1] = worldTransform_.scale_.y;
+	matScale.m[2][2] = worldTransform_.scale_.z;
+	matScale.m[3][3] =1;
+
+#pragma endregion
+
+#pragma region 回転角
+
+	worldTransform_.rotation_ = { MathUtility::PI / 4.0f,MathUtility::PI / 4.0f,0.0f };//回転角を設定
+	Matrix4 matRotZ, matRotX, matRotY;
+	//Z軸回転行列の各要素を設定する
+	matRotZ.m[0][0] =cosf(worldTransform_.rotation_.z);
+	matRotZ.m[0][1] = sinf(worldTransform_.rotation_.z);
+	matRotZ.m[1][0] = -sinf(worldTransform_.rotation_.z);
+	matRotZ.m[1][1] = cosf(worldTransform_.rotation_.z);
+	matRotZ.m[2][2] = 1;
+	matRotZ.m[3][3] = 1;
+	
+	 
+    //X軸回転行列の各要素を設定する
+	matRotX.m[0][0] = 1;
+	matRotX.m[1][1] = cosf(worldTransform_.rotation_.x);
+	matRotX.m[1][2] = sinf(worldTransform_.rotation_.x);
+	matRotX.m[2][1] = -sinf(worldTransform_.rotation_.x);
+	matRotX.m[2][2] = cosf(worldTransform_.rotation_.x);
+	matRotX.m[3][3] = 1;
+	 
+	//Y軸回転行列の各要素を設定する
+	matRotY.m[0][0] = cosf(worldTransform_.rotation_.y);
+	matRotY.m[1][1] = 1;
+	matRotY.m[0][2] = -sinf(worldTransform_.rotation_.y);
+	matRotY.m[2][0] = sinf(worldTransform_.rotation_.y);
+	matRotY.m[2][2] = cosf(worldTransform_.rotation_.y);
+	matRotY.m[3][3] = 1;
+
+	
+	
+	
+#pragma endregion 	
+	//x,y,z軸周りの平行移動を設定
+	worldTransform_.translation_ = {10,10,10 };
+	//平行移動行列を宣言
 	Matrix4 matTrans = MathUtility::Matrix4Identity();
 
-	//スケーリング倍率を行列に設定する
-	matScale.m[0][0] =sin(worldTransform_.scale_.x);
-	//matScale.m[0][1] = worldTransform_.scale_.y;
-	//matScale.m[1][1] = worldTransform_.scale_.z;
-	//Z軸回転行列の各要素を設定する
-	//matRotZ.m[0][0] = worldTransform_.rotation_.x;
-	//matRotZ.m[0][1] = worldTransform_.rotation_.y;
-	matRotZ.m[0][0] = cos(worldTransform_.rotation_.z);
-	
-    //X軸回転行列の各要素を設定する
-	matRotX.m[0][0] = worldTransform_.rotation_.x;
-	//matRotX.m[0][1] = worldTransform_.rotation_.y;
-	//matRotX.m[1][0] = worldTransform_.rotation_.z;
-
-	//Y軸回転行列の各要素を設定する
-	//matRotY.m[0][0] = worldTransform_.rotation_.x;
-	matRotY.m[0][1] = worldTransform_.rotation_.y;
-	//matRotY.m[1][0] = worldTransform_.rotation_.z;
-	
 	//移動量を行列に設定する
-   
-	matTrans.m[0][0] = worldTransform_.translation_.y;
+	matTrans.m[0][0] = 1;
+	matTrans.m[1][1] = 1;
+	matTrans.m[2][2] = 1;
+	matTrans.m[3][3] = 1;
+	matTrans.m[3][0] = worldTransform_.translation_.x;
+	matTrans.m[3][1] = worldTransform_.translation_.y;
+	matTrans.m[3][2] = worldTransform_.translation_.z;
+
+
+
+	worldTransform_.matWorld_ = Matrix4(
+		1, 0, 0, 0,
+		0, 1, 0, 0,
+		0, 0, 1, 0,
+		0, 0, 0, 1
+	);
+
+
 	
-
-
-	worldTransform_.matWorld_ = matScale;//単位行列を代入
     worldTransform_.matWorld_*=matScale;//掛け算
 
-	worldTransform_.matWorld_ = matRotZ;//代入
+
 	worldTransform_.matWorld_ *= matRotZ;//掛け算
 
-	worldTransform_.matWorld_ = matRotX;//代入
+	
 	worldTransform_.matWorld_ *= matRotX;//掛け算
 
-	worldTransform_.matWorld_ = matRotY;//代入
+	
 	worldTransform_.matWorld_ *= matRotY;//掛け算
 
 
-	worldTransform_.matWorld_ = matTrans;
-	worldTransform_.matWorld_ *= matTrans;
-
+	
+	worldTransform_.matWorld_ *= matTrans;//掛け算
+	
 
 	
 	worldTransform_.TransferMatrix();//行列の転送
-	//PrimitiveDrawer::GetInstance()->SetViewProjection(&debugCamera_->GetViewProjection());//ライン描画が参照するビュープロジェクションを指定する(アドレス渡し
-	worldTransform_.Initialize();//ワールドトランスフォームの初期化
-	viewProjection_.Initialize();//ビュープロジェクションの初期化
 }
 
 void GameScene::Update() {
@@ -89,7 +123,7 @@ void GameScene::Update() {
 
 void GameScene::Draw() {
 
-	// コマンドリストの取得
+	// コマンドリストの取得								
 	ID3D12GraphicsCommandList* commandList = dxCommon_->GetCommandList();
 
 #pragma region 背景スプライト描画
@@ -116,16 +150,8 @@ void GameScene::Draw() {
 
 	//model_->Draw(worldTransform_,viewProjection_,textureHandle_);//3Dモデル描画
 	model_->Draw(worldTransform_, debugCamera_->GetViewProjection(), textureHandle_);
-	//PrimitiveDrawer::GetInstance()->DrawLine3d(Vector3(0,10,0),Vector3(0,-10,0),Vector4(0,0,100,100));//ライン描画が参照するビュープロジェクションを指定する(アドレス渡し)
-	//PrimitiveDrawer::GetInstance()->DrawLine3d(Vector3(-10, 10, 0), Vector3(-10, -10, 0), Vector4(0, 0, 100, 100));
-	//PrimitiveDrawer::GetInstance()->DrawLine3d(Vector3(10, 10, 0), Vector3(10, -10, 0), Vector4(0, 0, 100, 100));
-	//PrimitiveDrawer::GetInstance()->DrawLine3d(Vector3(5, 10, 0), Vector3(5, -10, 0), Vector4(0, 0, 100, 100));
-	//PrimitiveDrawer::GetInstance()->DrawLine3d(Vector3(-5, 10, 0), Vector3(-5, -10, 0), Vector4(0, 0, 100, 100));
-	//PrimitiveDrawer::GetInstance()->DrawLine3d(Vector3(10, 0, 0), Vector3(-10, 0, 0), Vector4(100, 0, 0, 100));
-	//PrimitiveDrawer::GetInstance()->DrawLine3d(Vector3(10, 10, 0), Vector3(-10, 10, 0), Vector4(100, 0, 0, 100));
-	//PrimitiveDrawer::GetInstance()->DrawLine3d(Vector3(10, -10, 0), Vector3(-10, -10, 0), Vector4(100, 0, 0, 100));
-	//PrimitiveDrawer::GetInstance()->DrawLine3d(Vector3(10, 5, 0), Vector3(-10, 5, 0), Vector4(100, 0, 0, 100));
-	//PrimitiveDrawer::GetInstance()->DrawLine3d(Vector3(10, -5, 0), Vector3(-10, -5, 0), Vector4(100, 0, 0, 100));
+	
+
 	Model::PostDraw();// 3Dオブジェクト描画後処理																									
 #pragma endregion
 
