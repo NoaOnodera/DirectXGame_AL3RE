26c0db0f9ce199d78c7b8ad82babc66c984d24ed
乃碧 小野寺 <deranoir102493@gmail.com>
2022-05-31 13:25:31 +0900

    確認課題04_RE

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -3,6 +3,7 @@
 #include "AxisIndicator.h"
 #include <cassert>
 #include <random>
+#include <DirectXMath.h>
 
 GameScene::GameScene() {}
 
@@ -40,28 +41,16 @@ void GameScene::Initialize() {
 	
 	for (WorldTransform& worldTransform : worldTransforms_) {
 
-		//カメラ視点座標を設定
-		viewProjection_.eye = {0,0,-10};
+		
 		//カメラ注視点座標を設定
-		viewProjection_.target = { 10,0,0 };
+		//viewProjection_.target = { 10,0,0 };
 		//カメラ上方向ベクトルを設定(右上45度指定)
-		viewProjection_.up = { cosf(MathUtility::PI / 4.0f),sinf(MathUtility::PI / 4.0f),0.0f };
-
-		//ワールドトランスフォームの初期化
-		worldTransform.Initialize();
-		////X,Y,Z方向のスケーリングを設定
-		//worldTransform.scale_ = { 1,1,1 };
-		////X,Y,Z軸周りの回転角を乱数で設定
-		//worldTransform.rotation_ = { rotDist(engine),rotDist(engine),rotDist(engine) };
-		////X,Y,Z軸周りの平行移動を乱数で設定
-		//worldTransform.translation_ = { posDist(engine),posDist(engine),posDist(engine) };
-
-
+		//viewProjection_.up = { cosf(MathUtility::PI / 4.0f),sinf(MathUtility::PI / 4.0f),0.0f };
 
 		
-
-
-
+		//ワールドトランスフォームの初期化
+		worldTransform.Initialize();
+		
 #pragma region スケーリング
 	//スケーリング
 		worldTransform.scale_ = { 1,1,1 };//x,y,z方向のスケーリングを設定
@@ -163,73 +152,61 @@ void GameScene::Initialize() {
 			//行列の転送
 		worldTransform.TransferMatrix();
 	}
+#pragma region 垂直 
+
+	//カメラ垂直方向視野角を設定
+	viewProjection_.fovAngleY = DirectX::XMConvertToRadians(50.0f);
+
+	//aspectRation
+	viewProjection_.aspectRatio = 1.0f;
+	//ニアクリップ距離を設定
+	viewProjection_.nearZ = 52.0f;
+	//ファークリップ距離を設定
+	viewProjection_.farZ = 53.0f;
+
+
+#pragma endregion
 	
 	viewProjection_.Initialize();
 }
 
 void GameScene::Update() {
-#pragma region 視点
-	//視点の移動ベクトル
-	Vector3 moveEye = { 0,0,0 };
-	//視点の移動速さ
-	const float kEyeSpeed = 0.2f;
-	//押した方向で移動ベクトルを変更
-	if (input_->PushKey(DIK_W)) {
-		moveEye = { 0,0,kEyeSpeed };
-	}
-	else if (input_->PushKey(DIK_S)) {
-		moveEye = { 0,0,-kEyeSpeed };
-		}
+	////上キーで視野角が広がる
+	//if (input_->PushKey(DIK_W)) {
+	//	viewProjection_.fovAngleY += 0.01f;
+	//	viewProjection_.fovAngleY = min(viewProjection_.fovAngleY, MathUtility::PI);
 
-	//視点移動(ベクトルの加算)
-	viewProjection_.eye += moveEye;
-	//行列の再計算
-	viewProjection_.UpdateMatrix();
-	//デバック用表示
-	debugText_->SetPos(50, 50);
-	debugText_->Printf("eye:(% f, % f, % f)", viewProjection_.eye.x, viewProjection_.eye.y, viewProjection_.eye.z);
-#pragma endregion
+	//}
+	//else if (input_->PushKey(DIK_S)) {
+	//	viewProjection_.fovAngleY -= 0.01f;
+	//	viewProjection_.fovAngleY = max(viewProjection_.fovAngleY, 0.01f);
+	//}
+	////行列の再計算
+	//viewProjection_.UpdateMatrix();
 
-#pragma region 注視点
-	//注視点の移動ベクトル
-	Vector3 moveTarget = { 0,0,0 };
-	//注視点の移動速さ
-	const float kTargetSpeed = 0.2f;
-	//押した方向で移動ベクトルを変更
-	if (input_->PushKey(DIK_LEFT)) {
-		moveTarget = { -kTargetSpeed,0,0 };
+	////デバッグ用表示
+	//debugText_->SetPos(50, 110);
+	//debugText_->Printf("fovAngleY(Degree):%f", DirectX::XMConvertToDegrees(viewProjection_.fovAngleY));
+	//
+	//上下キーでニアクリップ距離を増減
+	if (input_->PushKey(DIK_UP)) {
+		viewProjection_.nearZ += 0.1f;
 	}
-	else if (input_ -> PushKey(DIK_RIGHT)) {
-		moveTarget = { kTargetSpeed,0,0 };
+	else if (input_->PushKey(DIK_DOWN)) {
+		viewProjection_.nearZ -= 0.1f;
 	}
-	//注視点移動(ベクトルの加算)
-	viewProjection_.target += moveTarget;
-
-	//行列の再計算
-	viewProjection_.UpdateMatrix();
-	//デバック用表示
-	debugText_->SetPos(50, 70);
-	debugText_->Printf("target:(%f,%f,%f)", viewProjection_.target.x, viewProjection_.target.y, viewProjection_.target.z);
-#pragma endregion 
-#pragma region 連続回転
-	//上方向回転処理
-	//上方向の回転速さ[ラジアン/frame]
-	const float kUpRotSpeed = 0.05f;
-	//押した方向で移動ベクトルを変更
-	if (input_->PushKey(DIK_SPACE)) {
-		viewAngle += kUpRotSpeed;
-      //2πを超えたら0に戻す
-		viewAngle = fmodf(viewAngle, MathUtility::PI * 2.0f);
-	}
-	//上方向ベクトルを計算(半径1の円周上の座標)
-	viewProjection_.up = { cosf(viewAngle),sinf(viewAngle),0.0f };
-	//行列の再計算
-	viewProjection_.UpdateMatrix();
-	//デバッグ用表示
-	debugText_->SetPos(50, 90);
-	debugText_->Printf("up:(%f,%f,%f)", viewProjection_.up.x, viewProjection_.up.y, viewProjection_.up.z);
-
 	
+	//行列の再計算
+	viewProjection_.UpdateMatrix();
+
+
+	//デバッグ用表示
+	debugText_->SetPos(50, 130);
+	debugText_->Printf("nearZ:%f", viewProjection_.nearZ);
+	
+	
+	debugText_->SetPos(50, 50);
+	debugText_->Printf("eye:(%f,%f,%f)", viewProjection_.eye.x,viewProjection_.eye.y,viewProjection_.eye.z);
 
 	//debugCamera_->Update();//デバッグカメラの更新
 }
