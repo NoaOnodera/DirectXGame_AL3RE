bb948b5889b562a8298379a4829590dcf7247bff
乃碧 小野寺 <deranoir102493@gmail.com>
2022-06-21 17:47:18 +0900

    03_07_0.1

---
+++ EnemyBullet.cpp
@@ -0,0 +1,50 @@
+#include "Enemy.h"
+#include "EnemyBullet.h"
+#include<cassert>
+
+
+
+EnemyBullet::EnemyBullet() {
+
+}
+
+
+EnemyBullet::~EnemyBullet() {
+	delete vectorMove_;
+}
+
+void EnemyBullet::Initialize(Model* model, const Vector3& position, const Vector3& velocity) {
+	//NULLポインタチェック
+	assert(model);
+
+	model_ = model;
+	//テクスチャ読み込み
+	textureHandle_ = TextureManager::Load("player_bullet.jpg");
+
+
+	//ワールドトランスフォームの初期化
+	worldTransform_.Initialize();
+	//引数で受け取った初期座標をセット
+	worldTransform_.translation_ = position;
+
+	vectorMove_ = new VectorMove();
+
+
+	//引数で受け取った速度をメンバ変数に代入
+	velocity_ = velocity;
+}
+
+void EnemyBullet::Update() {
+	vectorMove_->MyUpdate(worldTransform_);
+	//座標を移動させる(1フレーム分の移動力を足し込む)
+	worldTransform_.translation_ += velocity_;
+	//時間経過でdeath
+	if (--deathTimer_ <= 0) {
+		isDead_ = true;
+	}
+
+}
+void EnemyBullet::Draw(const ViewProjection& viewProjection) {
+	//モデルの描画
+	model_->Draw(worldTransform_, viewProjection, textureHandle_);
+}
---
+++ EnemyBullet.h
@@ -0,0 +1,35 @@
+#pragma once
+#include "Model.h"
+#include "WorldTransform.h"
+#include"TextureManager.h"
+#include "VectorMove.h"
+#include "DebugText.h"
+
+class EnemyBullet {
+public:
+	EnemyBullet();
+	~EnemyBullet();
+	void Initialize(Model* model, const Vector3& position, const Vector3& velocity);
+	void Update();
+	void Draw(const ViewProjection& viewProjection);
+	bool IsDead() const { return isDead_; }
+private:
+
+	//ワールド変換データ
+	WorldTransform worldTransform_;
+	//モデルのポインタ(借りてくる)
+	Model* model_ = nullptr;
+	//テクスチャハンドル
+	uint32_t textureHandle_ = 0u;
+	//速度
+	Vector3 velocity_;
+
+	DebugText* debugText_ = nullptr;
+	VectorMove* vectorMove_ = nullptr;
+	//寿命<frm>
+	static const int32_t kLifeTime = 60 * 5;
+	//デスタイマー
+	int32_t deathTimer_ = kLifeTime;
+	//デスフラグ
+	bool isDead_ = false;
+};
